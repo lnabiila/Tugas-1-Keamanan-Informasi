c0b2d93cbbd47f7120868ce3ce89b0670dfabdb2
Iana <117833447+lnabiila@users.noreply.github.com>
2024-10-09 09:41:59 +0700

    revisi

--- Tugas1KI.py
+++ Tugas1KI.py
@@ -1,6 +1,6 @@
 import random
 
-initalPermutation = [58, 50, 42, 34, 26, 18, 10, 2,
+initialPermutation = [58, 50, 42, 34, 26, 18, 10, 2,
                     60, 52, 44, 36, 28, 20, 12, 4,
                     62, 54, 46, 38, 30, 22, 14, 6,
                     64, 56, 48, 40, 32, 24, 16, 8,
@@ -34,17 +34,89 @@ straightPermutation = [16,  7, 20, 21,
                         19, 13, 30,  6,
                         22, 11,  4, 25]
 
+sBox = [[[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+         [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+         [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+         [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]],
+ 
+        [[15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+         [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+         [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+         [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]],
+ 
+        [[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+         [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+         [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+         [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]],
+ 
+        [[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+         [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+         [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+         [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]],
+ 
+        [[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+         [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+         [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+         [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]],
+ 
+        [[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+         [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+         [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+         [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]],
+ 
+        [[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+         [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+         [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+         [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]],
+ 
+        [[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+         [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+         [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+         [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]]]
+
+keyParity = [49, 57, 41, 33, 25, 17, 9,
+            1, 58, 50, 42, 34, 26, 18,
+            10, 2, 59, 51, 43, 35, 27,
+            19, 11, 3, 60, 52, 44, 36,
+            63, 55, 47, 39, 31, 23, 15,
+            7, 62, 54, 46, 38, 30, 22,
+            14, 6, 61, 53, 45, 37, 29,
+            21, 13, 5, 28, 20, 12, 4]
+
+keyCompression = [14, 17, 11, 24, 1, 5,
+                    3, 28, 15, 6, 21, 10,
+                    23, 19, 12, 4, 26, 8,
+                    16, 7, 27, 20, 13, 2,
+                    41, 52, 31, 37, 47, 55,
+                    30, 40, 51, 45, 33, 48,
+                    44, 49, 39, 56, 34, 53,
+                    46, 42, 50, 36, 29, 32]
+
+leftShifts = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
+
 def binerHex(teks):
     angka = int(teks, 2)
-    return(hex(angka))
+    return hex(angka)[2:].zfill(16)
 
 def hexBiner(teks):
     angka = int(teks, 16)
     biner = bin(angka)[2:]
     return biner.zfill(64)
 
-def permutasi(blok, table):
-    return ''.join(blok[i - 1] for i in table)
+def binerString(teks):
+    return ''.join(chr(int(teks[i:i+8], 2)) for i in range(0, len(teks), 8)).rstrip()
+
+def stringBiner(teks):
+    return ''.join(format(ord(c), '08b') for c in teks.ljust(8))
+
+def permutasi(k, arr, n):
+    permutation = ""
+    for i in range(0, n):
+        permutation = permutation + k[arr[i] - 1]
+    return permutation
+
+def leftShift(kunci, shift):
+    return kunci[shift:] + kunci[:shift]
 
 def xor(a, b):
     tmp = ""
@@ -53,39 +125,92 @@ def xor(a, b):
         else: tmp += "1"
     return tmp
 
-def fungsiFeistel(kanan, key):
-    ekspansiKanan = permutasi(kanan, expansionDBox)
-    hasilXor = xor(ekspansiKanan, key)
-    return permutasi(hasilXor, straightPermutation)
+def enkripsi(teks, rkb, rk):
+    blokBiner = stringBiner(teks)
+    blokBaru = permutasi(blokBiner, initialPermutation, 64)
+    kiri, kanan = blokBaru[:32], blokBaru[32:]
+    for i in range(0, 16):
+        ekspansiKanan = permutasi(kanan, expansionDBox, 48)
+        hasilXor = xor(ekspansiKanan, rkb[i])
 
-def putaran(kiri, kanan, key):
-    kananBaru = xor(kiri, fungsiFeistel(kanan, key))
-    return kanan, kananBaru
+        substitusiSBox = ""
+        for j in range(0, 8):
+            baris = int(hasilXor[j * 6] + hasilXor[j * 6 + 5], 2)  
+            kolom = int(hasilXor[j * 6 + 1:j * 6 + 5], 2)          
+            val = sBox[j][baris][kolom]
+            substitusiSBox += bin(val)[2:].zfill(4)
 
-def blokData(blok, key):
-    blok = permutasi(blok, initalPermutation)
-    kiri, kanan = blok[:32], blok[32:]
-    for _ in range(16): kiri, kanan = putaran(kiri, kanan, key)
-    blokBaru = kanan + kiri
-    return permutasi(blokBaru, finalPermutation)
+        substitusiSBox = permutasi(substitusiSBox, straightPermutation, 32) 
+        hasil = xor(kiri, substitusiSBox)
+        kiri = hasil
+        if(i != 15): kiri, kanan = kanan, kiri
+    combine = kiri + kanan
+    teksEnkripsi = permutasi(combine, finalPermutation, 64)
+    return teksEnkripsi
 
-def enkripsi(teks, key):
-    blokBiner = ''.join(format(ord(c), '08b') for c in teks.ljust(8))
-    blokEnkripsi = blokData(blokBiner, key)
-    return binerHex(blokEnkripsi)
+def decrypt(teks, rkb_rev, rk_rev):
+    blokBaru = permutasi(teks, initialPermutation, 64)
+    kiri, kanan = blokBaru[:32], blokBaru[32:]
+    for i in range(0, 16):
+        ekspansiKanan = permutasi(kanan, expansionDBox, 48)
+        hasilXor = xor(ekspansiKanan, rkb_rev[i])
 
-def dekripsi(teksEnkripsi, key):
-    blokBiner = hexBiner(teksEnkripsi)
-    blokDekripsi = blokData(blokBiner, key)
-    return ''.join(chr(int(blokDekripsi[i:i+8], 2)) for i in range(0, len(blokDekripsi), 8)).rstrip()
+        substitusiSBox = ""
+        for j in range(0, 8):
+            baris = int(hasilXor[j * 6] + hasilXor[j * 6 + 5], 2)  
+            kolom = int(hasilXor[j * 6 + 1:j * 6 + 5], 2)          
+            val = sBox[j][baris][kolom]
+            substitusiSBox += bin(val)[2:].zfill(4)
+
+        substitusiSBox = permutasi(substitusiSBox, straightPermutation, 32) 
+        hasil = xor(kiri, substitusiSBox)
+        kiri = hasil
+        if(i != 15): kiri, kanan = kanan, kiri
+    blokBaru = kiri + kanan
+    teksDekripsi = permutasi(blokBaru, finalPermutation, 64)
+    return teksDekripsi
+
+def teksEnkripsi(pt, rkb, rk):
+    teksEnkripsii = ""
+    for i in range(0, len(pt), 8):
+        blok = pt[i:i+8]
+        blokEnkripsi = enkripsi(blok, rkb, rk)
+        teksEnkripsii += blokEnkripsi
+    return teksEnkripsii
+
+def teksDekripsi(ct, rkb_rev, rk_rev):
+    teksAsli = ""
+    for i in range(0, len(ct), 64): 
+        blok = ct[i:i+64]
+        blokTeks = decrypt(blok, rkb_rev, rk_rev) 
+        
+        teksAsli += binerString(blokTeks)
+    return teksAsli
 
 def keyGenerator():
-    return ''.join(random.choice('01') for _ in range(48))
+    return ''.join(random.choice('01') for _ in range(64))
 
-teks1 = input("Teks yang ingin dienkripsi (maks 8 karakter): ")
-key = keyGenerator() 
-print("Generated Key:", binerHex(key))
-print("Hasil Enkripsi:", enkripsi(teks1, key))
+key = hexBiner(keyGenerator())
+key = permutasi(key, keyParity, 56)
+kiri, kanan = key[:28], key[28:]
+ 
+rkb = []
+rk = []
+for i in range(0, 16):
+    kiri = leftShift(kiri, leftShifts[i])
+    kanan = leftShift(kanan, leftShifts[i])
 
-teks2 = input("Teks yang ingin didekripsi: ")
-print("Hasil Dekripsi:", dekripsi(teks2, key))
+    blokBaru = kiri + kanan
+    putaran = permutasi(blokBaru, keyCompression, 48)
+    
+    rkb.append(putaran)
+    rk.append(binerHex(putaran))
+
+teks = input("Teks yang ingin dienkripsi (maks 8 karakter): ")
+teksEnkripsii = teksEnkripsi(teks, rkb, rk)
+print("Hasil Enkripsi:", binerHex(teksEnkripsii))
+
+rkb_rev = rkb[::-1]
+rk_rev = rk[::-1]
+teksAsli = teksDekripsi(teksEnkripsii, rkb_rev, rk_rev)
+print("Hasil Dekripsi: ", teksAsli)
